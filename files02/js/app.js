f5ed8ee53b21cc09174506e1f1acef24d5f0990f
VICTORIA_KANICKA <vkanicka@gmail.com>
2021-02-07 14:42:58 -0600

    Avatar now changes expression based on events and stats. Social actions built out. Exercise actions built out. Next steps: Build out learning actions. Build out shop actions, but may want to build out shop overlay screen first. Can start adding images to social/career/exercise events. Will need to adjust ++points so bars don't max out too easily. Will want to start thinking about how to build out bad luck / misfortune. Will also want to get turn time constraints sorted out.

--- files02/js/app.js
+++ files02/js/app.js
@@ -53,102 +53,6 @@ const randomizeColor=()=>{
   return `rgb(${rando(150,255)},${rando(100,255)},${rando(0,255)}`
 }
 
-const jobMarket = {
-  barista: {
-    title: 'Coffee Barista',
-    id: 'barista',
-    pay: 400,
-    happiness: -5,
-    skills: 1,
-    time: 8,
-    xp: 0
-  },
-  manager: {
-    title: 'Coffee Shop Manager',
-    id: 'manager',
-    pay: 610,
-    happiness: -8,
-    skills: 1,
-    time: 8,
-    xp: 5
-  },
-  dataEntry: {
-    title: 'Part-Time Data Entry Specialist',
-    id: 'dataEntry',
-    pay: 250,
-    happiness: -8,
-    skills: 1,
-    time: 4,
-    xp: 10
-  },
-  seniorDataEntry: {
-    title: 'Part-Time Data Entry Associate',
-    id: 'seniorDataEntry',
-    pay: 300,
-    happiness: -1,
-    skills: 1,
-    time: 4,
-    xp: 15
-  },
-  juniorDev: {
-    title: 'Junior Software Developer',
-    id: 'juniorDev',
-    pay: 1250,
-    happiness: +1,
-    skills: 1,
-    time: 8,
-    xp: 20
-  },
-  midDev: {
-    title: 'Mid Level Software Developer',
-    id: 'midDev',
-    pay: 1731,
-    happiness: +5,
-    skills: 1,
-    time: 8,
-    xp: 25
-  },
-  seniorDev: {
-    title: 'Senior Software Developer',
-    id: 'seniorDev',
-    pay: 2308,
-    happiness: +10,
-    skills: 1,
-    time: 8,
-    xp: 30
-  },
-  cto: {
-    title: 'CTO',
-    id: 'cto',
-    pay: 10416,
-    happiness: +5,
-    skills: 1,
-    time: 8,
-    xp: 35
-  }
-}
-
-const store =  {
-
-}
-
-const learningLibrary = {
-
-}
-
-const exerciseLibrary = {
-  level1: {name: 'Free Beginner Yoga Video', health: 5, happiness: 5, cost: 0, social: 0}
-}
-
-const people = {
-
-}
-
-const relaxation =  {
-
-}
-
-
 const game = {
 
   avatar: {
@@ -156,38 +60,73 @@ const game = {
           wealth: 0,
           social: 0,
           health: 20,
-          happiness: 0,
-          skills: 0
+          happiness: 6,
+          skills: 0,
+
+          workXP: 0,
+          exerciseXP: 0,
+          socialXP:0
+      },
+      moodImages: {
+        default: 'https://i.imgur.com/fkCNTjU.png',
+        sad:'https://i.imgur.com/azfboBa.png',
+        happy: 'https://i.imgur.com/NCsLg5m.png',
+        relaxed: 'https://i.imgur.com/0GHECUi.png'
+      },
+      updateImg: (state)=>{
+        if(typeof(state)!='undefined'){
+            state=state
+        }
+        else if (game.avatar.perf.happiness<=5) {
+            state='sad'
+        }
+        else {
+          state='default'
+        }
+        $('#avatar').attr('src',game.avatar.moodImages[state])
+
+        //change image
+
       },
       inventory:{
 
       },
 
-      job: jobMarket.barista,
+      job: 'unemployed',
       getJob: (jobTitle)=>{
-        game.avatar.job = jobMarket[jobTitle]
+        game.avatar.job = game.jobMarket[jobTitle]
         $('#jobTitleDisplay').text(`Job: ${game.avatar.job.title}`)
         $('#jobPayDisplay').text(`Pay: $${game.avatar.job.pay}/wk`)
       },
       getPromotion: ()=>{
         if(game.avatar.job.title !='CTO') {
-          if(game.avatar.perf.skills === jobMarket[Object.keys(jobMarket)[Object.keys(jobMarket).indexOf(game.avatar.job.id)+1]].xp) {game.avatar.getJob(( jobMarket[Object.keys(jobMarket)[Object.keys(jobMarket).indexOf(game.avatar.job.id)+1]]).id)
-          alert(`Congrats! You've been promoted to ${game.avatar.job.title}!`)}
+          if(game.avatar.perf.workXP === game.jobMarket[Object.keys(game.jobMarket)[Object.keys(game.jobMarket).indexOf(game.avatar.job.id)+1]].workXP) {game.avatar.getJob((game.jobMarket[Object.keys(game.jobMarket)[Object.keys(game.jobMarket).indexOf(game.avatar.job.id)+1]]).id)
+          game.avatar.updateImg('happy')
+          alert(`Congrats! Alita has been promoted to ${game.avatar.job.title}!`)}
         }
       },
 
-      relationships:{
+      exerciseLevel: (''),
+      getExerciseLevel: (levelNumber)=>{
+        game.avatar.exerciseLevel = game.exerciseLibrary[levelNumber]
+
+      },
+      socialLevel:'',
+      getSocialLevel: (levelNumber)=>{
+        game.avatar.socialLevel = game.socialLibrary[levelNumber]
 
       },
 
-      exerciseLevel: exerciseLibrary.level1,
-
-      skills: {
+      skillLevel: '',
+      getSkillLevel: (levelNumber)=>{
+        game.avatar.skillLevel = game.skillLibrary[levelNumber]
 
       },
-      milestones: {
 
-      }
+
+      walkIn: () => {
+        $thisAvatar = $('#avatar')
+        $thisAvatar.css({'left':'400px','transition':'3s','steps':('3', 'jump-start')})},
   },
   phone: {
     clock: {
@@ -224,44 +163,201 @@ const game = {
       $('#skillsMeter').attr('value',game.avatar.perf.skills)
 
       $('#bankAccountDisplay').text(`Bank Account: $${game.avatar.perf.wealth}`)
-      $('#workXPDisplay').text(`WorkXP: ${game.avatar.perf.skills}`)
+      $('#workXPDisplay').text(`WorkXP: ${game.avatar.perf.workXP}`)
     },
     work: ()=>{
-      game.avatar.perf.wealth+=game.avatar.job.pay
-      game.avatar.perf.happiness+=game.avatar.job.happiness
-      game.avatar.perf.skills+=game.avatar.job.skills
-      game.phone.incrementClock(8)
-      game.phone.updateMeters()
-      setTimeout(function(){game.avatar.getPromotion()},100)
-
+      if(game.avatar.perf.happiness<=5) {alert(`Alita is too depressed to work.`)}
+      else if (game.avatar.perf.health <=5) {alert(`Alita isn't feeling physically well enough to work.`)}
+      else {
+          game.avatar.perf.wealth+=game.avatar.job.pay
+          game.avatar.perf.happiness+=game.avatar.job.happiness
+          game.avatar.perf.workXP+=1
+          game.phone.incrementClock(game.avatar.job.time)
+          game.phone.updateMeters()
+          game.avatar.updateImg()
+          setTimeout(function(){game.avatar.getPromotion()},100)
+      }
     },
     shop: ()=>{
 
       game.phone.updateMeters()
+      game.avatar.updateImg()
     },
     learn: ()=>{
-      game.avatar.perf.skills+=1
-      game.phone.incrementClock(1)
+      if (
+        game.avatar.perf.happiness > 0
+        && game.avatar.perf.social >0
+        && game.avatar.perf.health > 0)
+        {
+        game.avatar.perf.skills+=250
+        game.phone.incrementClock(1)
+        game.phone.updateMeters()
+        game.avatar.updateImg()
+      } else {alert(`Alita can't concentrate on learning anything right now. Try to help her feel better.`)}
 
-      game.phone.updateMeters()
     },
     exercise: ()=>{
-      game.avatar.perf.health+=game.avatar.exerciseLevel.health
-      game.avatar.perf.wealth+=game.avatar.exerciseLevel.cost
-      game.avatar.perf.happiness+=game.avatar.exerciseLevel.happiness
-      game.phone.incrementClock(1)
-      game.phone.updateMeters()
+      if(game.avatar.perf.social <=0) {alert(`Alita is lonely. She can't motivate herself to exercise.`)}
+      else if (game.avatar.perf.health <=5) {alert(`Alita is not feeling physically well enough to work out right now.`)}
+      else {
+        game.avatar.perf.health+=game.avatar.exerciseLevel.health
+        game.avatar.perf.wealth+=game.avatar.exerciseLevel.cost
+        game.avatar.perf.happiness+=game.avatar.exerciseLevel.happiness
+        game.avatar.perf.exerciseXP+=1
+        game.phone.incrementClock(2)
+        game.phone.updateMeters()
+        game.avatar.updateImg()
+        if(game.avatar.perf.exerciseXP%3===0) {
+          alert(`Alita mastered ${game.avatar.exerciseLevel.name}!`)
+          game.avatar.exerciseLevel=game.exerciseLibrary[game.avatar.exerciseLevel.level+1]
+        }
+      }
     },
     socialize: ()=>{
-      game.avatar.perf.social+=5
-      game.avatar.perf.happiness+=5
-      game.phone.incrementClock(2)
-      game.phone.updateMeters()
+      if(game.avatar.perf.health<=5) {alert(`Alita is not feeling phsyically well enough to socialize.`)}
+      else {
+        p(game.avatar.socialLevel.name)
+        game.avatar.perf.social+=game.avatar.socialLevel.social
+        game.avatar.perf.happiness+=game.avatar.socialLevel.happiness
+        game.avatar.perf.wealth+=game.avatar.socialLevel.cost
+        game.avatar.perf.health+=game.avatar.socialLevel.health
+        game.avatar.perf.socialXP+=1
+        game.phone.incrementClock(2)
+        game.phone.updateMeters()
+        game.avatar.socialLevel=game.socialLibrary[game.avatar.socialLevel.level+1]
+        game.avatar.updateImg()
+      }
     },
     relax: ()=>{
       game.phone.calendar.newWeek()
       game.phone.updateMeters()
+      game.avatar.updateImg('relaxed')
     }
+  },
+  jobMarket: {
+    barista: {
+      title: 'Coffee Barista',
+      id: 'barista',
+      pay: 400,
+      happiness: -5,
+      skills: 1,
+      time: 8,
+      workXP: 0
+    },
+    manager: {
+      title: 'Coffee Shop Manager', // add photo - maybe
+      id: 'manager',
+      pay: 610,
+      happiness: -8,
+      skills: 1,
+      time: 8,
+      workXP: 5
+    },
+    dataEntry: {
+      title: 'Data Entry Specialist',
+      id: 'dataEntry',
+      pay: 250,
+      happiness: -8,
+      skills: 1,
+      time: 8,
+      workXP: 10
+    },
+    seniorDataEntry: {
+      title: 'Data Entry Associate',
+      id: 'seniorDataEntry',
+      pay: 300,
+      happiness: -1,
+      skills: 1,
+      time: 8,
+      workXP: 15
+    },
+    juniorDev: {
+      title: 'Junior Software Developer', // add photo
+      id: 'juniorDev',
+      pay: 1250,
+      happiness: +1,
+      skills: 1,
+      time: 8,
+      workXP: 20
+    },
+    midDev: {
+      title: 'Mid Level Software Developer',
+      id: 'midDev',
+      pay: 1731,
+      happiness: +5,
+      skills: 1,
+      time: 8,
+      workXP: 25
+    },
+    seniorDev: {
+      title: 'Senior Software Developer',
+      id: 'seniorDev',
+      pay: 2308,
+      happiness: +10,
+      skills: 1,
+      time: 8,
+      workXP: 30
+    },
+    cto: {
+      title: 'CTO', // add photo
+      id: 'cto',
+      pay: 10416,
+      happiness: +5,
+      skills: 1,
+      time: 8,
+      workXP: 35
+    }
+  },
+  store : {
+
+  },
+
+  learningLibrary: {
+
+  },
+
+  exerciseLibrary: {
+    0: {level: 0, name: 'Walking to the Park', health: 1, happiness: 1, cost: 0, social: 0},
+    1: {level: 1, name: 'Free Beginner Yoga Videos', health: 2, happiness: 2, cost: 5, social: 0}, // add photo
+    2: {level: 2, name: 'Free Basic Strength Training Videos', health: 3, happiness: 3, cost: 0, social: 0},
+    3: {level: 3, name: '5K Running Plan App', health: 5, happiness: 5, cost: 5, social: 0}, // add photo
+    4: {level: 4, name: 'Intermediate Yoga Video Subscription', health: 5, happiness: 5, cost: 0, social: 0},
+    5: {level: 5, name: 'Intermediate Strength Training Video Subscription', health: 5, happiness: 5, cost: 0, social: 0},
+    6: {level: 6, name: '10k Running Plan App', health: 5, happiness: 5, cost: 0, social: 0},
+    7: {level: 7, name: 'Advanced Yoga Class Subscription', health: 5, happiness: 5, cost: 0, social: 0},
+    8: {level: 8, name: 'Advanced Strength Training Video Subscription', health: 5, happiness: 5, cost: 0, social: 0}, // add photo
+    9: {level: 9, name: 'Half Marathon Running Plan App', health: 5, happiness: 5, cost: 0, social: 0},
+    10: {level: 10, name: 'Aerial Silks Yoga', health: 5, happiness: 5, cost: 0, social: 0}, // add photo
+    11: {level: 11, name: 'High Intensity Interval Training', health: 5, happiness: 5, cost: 0, social: 0},
+    12: {level: 12, name: 'Marathon Running Plan App', health: 5, happiness: 5, cost: 0, social: 0} // add photo
+  },
+
+  socialLibrary: {
+    0: {level: 0, social: 2, happiness:10, cost:0, health:0, name: 'Feeling overwhelmed, Alita called her mom, Lily, for advice. Her mom was supportive and encouraging.'}, // if buildout text screen - show mom texting her braver than you believe quote image
+    1: {level: 1, social: 8, happiness:5, cost: 10, health:1, name: 'Alita ventured out to a social bocce ball meetup. It was pretty fun.'},
+    2: {level: 2, social: 2, happiness:-5, cost: 0, health:0, name: 'Alita chatted on a dating app. Meh.'},
+    3: {level: 3, social: 50, happiness:50, cost: -25, health:0, name: 'Alita went to a murder mystery game meetup. It was so much fun! She made a new friend: Savannah. Alita and Savannah teamed up and guessed the murderer first. They each won $75.'}, //add photo to wall
+    4: {level: 4, social: 5, happiness:10, cost:10, health:8, name: 'Alita went to a yoga class with Savannah. They got to know each other better, and realized how much they have in common.'},
+    5: {level: 5, social: 50, happiness:0, cost:200, health: -40, name: "Alita went to a party with Savannah. They stayed out very late. Alita is now too tired and hungover to do anything. Take it easy today."},
+    6: {level: 6, social: 25, happiness:30, cost:0, health:0, name: 'Alita made a new friend at work: Falco. They both love coding.'},
+    7: {level: 7, social: 25, happiness:50, cost:300, health:10, name: 'Alita adopted a dog: Charlie!'}, // add photo
+    8: {level: 8, social: 10, happiness:10, cost:0, health:0, name: 'Alita and Falco went to a coding meetup group together.'},
+    9: {level: 9, social: 15, happiness:15, cost:0, health:20, name: 'Alita took Charlie to the dog park.'},
+    10: {level: 10, social: 25, happiness:20, cost:0, health:0, name: 'Savannah introduced Alita to a new friend: Sherlock.'},
+    11: {level: 11, social: 20, happiness:30, cost:50, health:0, name: 'Alita and Falco entered a coding competition as a team.'}, // add photo
+    12: {level: 12, social: 50, happiness:50, cost:0, health:0, name: 'Alita and Sherlock started dating.'}, // add photo
+    13: {level: 13, social: 15, happiness:20, cost:50, health:0, name: 'Alita and Sherlock went to the movies.'},
+    14: {level: 14, social: 45, happiness:30, cost:200, health:0, name: 'Alita and Sherlock went on a romantic dinner date.'}, // add photo
+    15: {level: 15, social: 20, happiness:15, cost:40, health:0, name: 'Alita and Sherlock hangout at a brewery with live music.'},
+    16: {level: 16, social: 100, happiness:100, cost:0, health:0, name: 'Sherlock proposed to Alita after hiking to a waterfall! Awww'}, // add photo
+    17: {level: 17, social: 100, happiness:100, cost:20000, health:0, name: 'Alita and Sherlock got married.'}, // add photo
+    18: {level: 18, social: 100, happiness:100, cost:10000, health:0, name: 'Alita and Sherlock went on a honeymoon to Italy.'}, // add photo
+    19: {level: 19, social: 50, happiness:100, cost:500, health:-50, name: 'Alita and Sherlock had a baby: Lily.'}, // add photo
+    20: {level: 20, social: 10, happiness:15, cost:0, health:10, name: 'Alita and Sherlock took Lily and Charlie to the park.'}
+  },
+
+  relaxation: {
+
   }
 }
 
@@ -277,6 +373,11 @@ $(() => {
   $('#relaxButton').on('click',game.phone.relax)
 
   game.avatar.getJob('barista')
+  game.avatar.getExerciseLevel(0)
+  game.avatar.getSocialLevel(0)
+  game.avatar.updateImg()
+  game.avatar.walkIn()
+
 
 
 
